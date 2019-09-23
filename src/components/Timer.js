f6dd6e727ef75203803518779e496be192a4319e
Vishnu Priya Bala Venkateshan <vishnupriya.b@jeanmartin.com>
2019-09-23 19:34:13 +0530

    avoid duplication and show the spinner

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,27 +3,48 @@ import React, { Component } from 'react';
 import './Home.css';
 import './Timer.css';
 import Timercard from '../hoc/Timercard';
+import { PacmanLoader
+} from 'react-spinners';
+import { css } from '@emotion/core';
 const nameText = [];
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    color: green;
+`;
 class Timer extends Component {
   constructor(props) {
     super(props);
-  }
-  componentDidMount() {
-    console.log(this.props);
+    this.state = {
+      isloading: true
+    }
   }
   render() {
-    nameText.push(this.props.name)
-    console.log(nameText);
-    const postList = nameText.length ? (
-      nameText.map((post, i) => {
-        //  post.filter((item) =>{
-          return (
-            <div className="card dynamic-card"  key={i}>
-              <h5>Hey {post}! Good morning</h5>
-            </div>
-          )
-        // });
-
+    nameText.push(this.props.name);
+    const uniqueNames = nameText.filter((val, id) => {
+      return nameText.indexOf(val) == id;
+    });
+    const postList = uniqueNames.length ? (
+      uniqueNames.map((post, i) => {
+        return (
+          <div className={i == (uniqueNames.length - 1) && ((uniqueNames.length - 1) < 6)  ? 'navigation-active' : 'navigation-inactive'} key={i}>
+            <div className="card">
+              <h6 className="arr-data">Hey {post}! Good morning
+              { i == (uniqueNames.length - 1) && ((uniqueNames.length - 1) < 6)  ? <div className='sweet-loading'>
+                <PacmanLoader
+                  css={override}
+                  sizeUnit={"px"}
+                  size={15}
+                  color={'#C62828'}
+                  isLoading={this.state.loading}
+                />
+              </div> : null }
+              
+              </h6> 
+              </div>             
+          </div>
+        )
       })
     ) : (
         <div>No data</div>
--- src/hoc/Timercard.js
+++ src/hoc/Timercard.js
@@ -2,9 +2,9 @@ import React from 'react';
 const textArray = ['Alex', 'Camran', 'Bruce', 'Nix', 'Aleo', 'Rano', 'Lilee'];
 
 const TimerCard = (WrappedComponent) => {
-    const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
-    const randomColor = colors[Math.floor(Math.random() * 5)];
-    const className = randomColor + '-text';
+    // const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
+    // const randomColor = colors[Math.floor(Math.random() * 5)];
+    // const className = randomColor + '-text';
     return class extends React.Component {
         constructor(props) {
             super(props);
@@ -13,19 +13,20 @@ const TimerCard = (WrappedComponent) => {
 
         componentDidMount() {
             this.timeout = setInterval(() => {
-                      let currentIdx = this.state.textIdx;
-                      this.setState({ textIdx: currentIdx + 1 });
-                    }, 1500);
+                let currentIdx = this.state.textIdx;
+                this.setState({ textIdx: currentIdx + 1 });
+            }, 1500);
         }
-          componentDidUnmount() {
-    clearInterval(this.timeout);
-  }
+        componentDidUnmount() {
+            clearInterval(this.timeout);
+        }
 
         render() {
             let textThatChanges = textArray[this.state.textIdx % textArray.length];
-            return(
-                <div className={className}>
-                    <WrappedComponent {...this.props} name={textThatChanges}/>
+            return (
+                <div>
+                    {/* <div className={className}> */}
+                    <WrappedComponent {...this.props} name={textThatChanges} />
                 </div>
             )
         }
